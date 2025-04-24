076c5e054268f52be1611f6ca1186838b846428f
ⓃⒺCⓉOR🍯 <167437180+drapterlagas@users.noreply.github.com>
2025-04-24 07:07:35 +0300

    Create config.cjs

---
+++ config.cjs
@@ -0,0 +1,31 @@
+// config.js
+const fs = require("fs");
+require("dotenv").config();
+
+const config = {
+  SESSION_ID: process.env.SESSION_ID || "Your Session Id",
+  PREFIX: process.env.PREFIX || '*',
+  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
+  AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
+  STATUS_READ_MSG: process.env.STATUS_READ_MSG || '',
+  AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
+  AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
+  AUTO_TYPING: process.env.AUTO_TYPING !== undefined ? process.env.AUTO_TYPING === 'true' : false,
+  AUTO_RECORDING: process.env.AUTO_RECORDING !== undefined ? process.env.AUTO_RECORDING === 'true' : false,
+  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE !== undefined ? process.env.ALWAYS_ONLINE === 'true' : false,
+  AUTO_REACT: process.env.AUTO_REACT !== undefined ? process.env.AUTO_REACT === 'true' : false,
+  AUTO_BLOCK: process.env.AUTO_BLOCK !== undefined ? process.env.AUTO_BLOCK === 'true' : true,
+  REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
+  NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
+  MODE: process.env.MODE || "public",
+  BOT_NAME: process.env.BOT_NAME || "BEE-NECTOR",
+  MENU_IMAGE: process.env.MENU_IMAGE || "https://files.catbox.moe/f70978.jpg",
+  DESCRIPTION: process.env.DESCRIPTION || "漏 鈩濔潝拣潝攫潝糕劃饾敾饾晩 BEE-NECTOR",
+  OWNER_NAME: process.env.OWNER_NAME || "ⓃⒺCⓉOR🍯",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "254791637868",
+  GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
+  WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
+};
+
+module.exports = config;
+    
